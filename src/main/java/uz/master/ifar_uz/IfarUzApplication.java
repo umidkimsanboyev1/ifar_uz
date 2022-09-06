450eec8adc7d6a82a269ee6905f2ba5ef5306ca4
umidkimsanboyev1 <umidmaster@yandex.com>
2022-09-06 11:06:21 +0500

    Init

---
+++ src/main/java/uz/master/ifar_uz/IfarUzApplication.java
@@ -0,0 +1,13 @@
+package uz.master.ifar_uz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfarUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfarUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/master/ifar_uz/IfarUzApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.master.ifar_uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfarUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
